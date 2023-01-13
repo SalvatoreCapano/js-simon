dbf06126b74c77aefc854b235f1523fc63200feb
Salvatore Capano <capano_salvatore@libero.it>
2023-01-13 14:31:57 +0100

    Agg. generazione 5 numeri casuali unici

--- js/script.js
+++ js/script.js
@@ -13,3 +13,47 @@
     -Stampare punteggio
     -Stampare array numeri indovinati 
 */
+
+const playBtn = document.querySelector("#playBtn");
+let numbersNumber = 5;
+
+
+playBtn.addEventListener("click", function(){
+
+    console.clear();
+
+    let numbers = [];
+
+    for (let i = 0; i <= numbersNumber; i++) {
+
+        numbers.push(getUniqueRandomNumber(numbers));
+
+    }
+    console.log("L'array da indovinare e'", numbers);
+
+});
+
+
+
+
+
+
+
+// FUNZIONI
+// Genera un numero casuale tra min e max
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Dato un array, genera un numero casuale non presente nell'array
+function getUniqueRandomNumber (array) {
+
+    let newRandomNumber;
+
+    do {
+        newRandomNumber = getRandomNumber(1, 500);
+    }
+    while (array.includes(newRandomNumber))
+
+    return newRandomNumber;
+}
