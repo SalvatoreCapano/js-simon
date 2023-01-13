65562db891e87cd80ab8fac66a91880a17ca01cc
Salvatore Capano <capano_salvatore@libero.it>
2023-01-13 14:08:08 +0100

    Agg. lista funzioni da implementare

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+-Generare 5 numeri casuali
+-Stampare i 5 numeri casuali
+
+-Dopo 30 secondi
+    -I numeri scompaiono
+    -Eseguire 5 volte:
+        -Inserire nel prompt i numeri in ordine
+        -Controllare se numero inserito = numero casuale
+            -se Vero: 
+                -punteggio++
+                -inserire numero inserito nell'array numeri indovinati
+    -Stampare punteggio
+    -Stampare array numeri indovinati 
+*/
